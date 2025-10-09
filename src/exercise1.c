af4bc6cd2a1cd02134a0e800413458353f125b6d
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:48:22 +0700

    ЧТО НЕ ТАК С ЭТИМ ОКРУГЛЕНИЕМ13

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,7 +3,7 @@ int main(int argc, char** argv) {
     float numbers[10];
     float sum = 0.0;
     for (int i = 0; i < 10; i++) {
-        scanf("%lf", &numbers[i]);
+        scanf("%f", &numbers[i]);
         sum += numbers[i];
     }
     float average = sum / 10.0;
