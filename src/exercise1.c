dce91ba991c7dc32212eb4645dfabc17f53eb036
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:40:49 +0700

    ЧТО НЕ ТАК С ЭТИМ ОКРУГЛЕНИЕМ9

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    double number;
     long long sum_cents = 0;
-
+    
+  
     for (int i = 0; i < 10; i++) {
+        double number;
         scanf("%lf", &number);
-
-        sum_cents += (long long)(number * 100 + (number >= 0 ? 0.5 : -0.5));
+  
+        long long cents;
+        if (number >= 0) {
+            cents = (long long)(number * 100 + 0.5);
+        } else {
+            cents = (long long)(number * 100 - 0.5);
+        }
+        sum_cents += cents;
     }
  
     long long average_cents = sum_cents / 10;
-    if (sum_cents % 10 >= 5) average_cents++;  
-    else if (sum_cents % 10 <= -5) average_cents--;  
     
 
+    long long remainder = sum_cents % 10;
+    if (remainder > 0 && remainder >= 5) average_cents++;
+    if (remainder < 0 && remainder <= -5) average_cents--;
+
     double average = average_cents / 100.0;
-    
     printf("%.2f\n", average);
     
     return 0;
