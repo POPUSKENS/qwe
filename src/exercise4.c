a62216797b2a54dce7cb6b5654befa87f1724ea0
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 17:39:36 +0700

    пятый тест

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define SIZE 10
+#define ROWS_A 10
+#define COLS_A 3
+#define ROWS_B 3
+#define COLS_B 10
 
-int main(int argc, char** argv) {
-    double numbers[SIZE];
-    double result[SIZE];
+int main() {
+    double matrixA[ROWS_A][COLS_A];
+    double matrixB[ROWS_B][COLS_B];
+    double result[ROWS_A][COLS_B];
 
-    for (int i = 0; i < SIZE; i++) {
-        scanf("%lf", &numbers[i]);
-    }
-
-    double X = atof(argv[1]);
-    int K = atoi(argv[2]);
-
-    for (int i = 0; i < SIZE; i++) {
-        if (i < K) {
-            result[i] = numbers[i];     
-        } else if (i == K) {
-            result[i] = X;              
-        } else {
-            result[i] = numbers[i - 1];  
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_A; j++) {
+            scanf("%lf", &matrixA[i][j]);
         }
     }
     
- 
-    for (int i = 0; i < SIZE; i++) {
-        printf("%g", result[i]);
-        if (i < SIZE - 1) {
-            printf(" ");
+
+    for (int i = 0; i < ROWS_B; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            scanf("%lf", &matrixB[i][j]);
+        }
+    }
+    
+
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            result[i][j] = 0;  
+            for (int k = 0; k < COLS_A; k++) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+    
+   
+    for (int i = 0; i < ROWS_A; i++) {
+        for (int j = 0; j < COLS_B; j++) {
+            printf("%g", result[i][j]);
+            if (!(i == ROWS_A - 1 && j == COLS_B - 1)) {
+                printf(" ");
+            }
         }
     }
     printf("\n");
