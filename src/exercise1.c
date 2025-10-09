60851071e59563686159617d5203e16aaf55babd
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:14:34 +0700

    сделано первое задание

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double numbers[10];
+    double sum = 0.0;
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &numbers[i]);
+        sum += numbers[i];
+    }
+    double average = sum / 10.0;
+    printf("%.2f\n", average);
     return 0;
 }
