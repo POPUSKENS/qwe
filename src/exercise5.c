b45910a650fa6f477224faba8020b3706a95b781
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 17:44:36 +0700

    пятый тест3

--- src/exercise5.c
+++ src/exercise5.c
@@ -10,35 +10,34 @@ int main() {
     double matrixB[ROWS_B][COLS_B];
     double result[ROWS_A][COLS_B];
     
-
+ 
     for (int i = 0; i < ROWS_A; i++) {
         for (int j = 0; j < COLS_A; j++) {
             scanf("%lf", &matrixA[i][j]);
         }
     }
     
-
     for (int i = 0; i < ROWS_B; i++) {
         for (int j = 0; j < COLS_B; j++) {
             scanf("%lf", &matrixB[i][j]);
         }
     }
     
-    
+  
     for (int i = 0; i < ROWS_A; i++) {
         for (int j = 0; j < COLS_B; j++) {
-            result[i][j] = 0; 
-            for (int k = 0; k < COLS_A; k++) {  
+            result[i][j] = 0;
+            for (int k = 0; k < COLS_A; k++) {
                 result[i][j] += matrixA[i][k] * matrixB[k][j];
             }
         }
     }
     
- 
+    
     for (int i = 0; i < ROWS_A; i++) {
         for (int j = 0; j < COLS_B; j++) {
-            printf("%g", result[i][j]);
-            if (!(i == ROWS_A - 1 && j == COLS_B - 1)) {
+            printf("%.2f", result[i][j]);  
+            if (i < ROWS_A - 1 || j < COLS_B - 1) {
                 printf(" ");
             }
         }
