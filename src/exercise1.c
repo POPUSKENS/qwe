cb3586e7fb8b34eab5bb632efc93fe7c0c92b7ac
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:20:22 +0700

    попытка исправить баг

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 int main(int argc, char** argv) {
     double numbers[10];
     double sum = 0.0;
@@ -8,7 +7,6 @@ int main(int argc, char** argv) {
         sum += numbers[i];
     }
     double average = sum / 10.0;
-    average = round(average * 100) / 100;
     printf("%.2f\n", average);
     return 0;
 }
