816ef4f7ff0ec9b4f122a354bbff4dd8f8ec97c4
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:24:42 +0700

    чёртава акругление как исправить помргите2

--- src/exercise1.c
+++ src/exercise1.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
         sum += numbers[i];
     }
     double average = sum / 10.0;
-    average += 0.0000001;
+    average += 0.01;
     printf("%.2f\n", average);
     return 0;
 }
