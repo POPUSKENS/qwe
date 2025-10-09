c6abdc14edf97db401d1fc605466ea8eacb4e5e8
Kryukov Kirill <krykir97@gmail.com>
2025-10-09 16:44:55 +0700

    ЧТО НЕ ТАК С ЭТИМ ОКРУГЛЕНИЕМ12

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(int argc, char** argv) {
-    double numbers[10];
-    double sum = 0.0;
+    float numbers[10];
+    float sum = 0.0;
     for (int i = 0; i < 10; i++) {
         scanf("%lf", &numbers[i]);
         sum += numbers[i];
     }
     float average = sum / 10.0;
-    average = double average
+    
     printf("%.2f\n", average);
     return 0;
 }
